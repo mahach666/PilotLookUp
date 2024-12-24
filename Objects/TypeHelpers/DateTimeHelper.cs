770a1dd3d82236f330f88d3326d7d4af06f16d73
Смирнов <maxah66@gmail.com>
2024-12-24 16:26:01 +0300

    DateTimeFix

--- Objects/TypeHelpers/DateTimeHelper.cs
+++ Objects/TypeHelpers/DateTimeHelper.cs
@@ -1,19 +1,15 @@
 ﻿using Ascon.Pilot.SDK;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PilotLookUp.Objects.TypeHelpers
 {
-    internal class DateTimeHelper : PilotObjectHelper
+    public class DateTimeHelper : PilotObjectHelper
     {
         public DateTimeHelper(DateTime obj, IObjectsRepository objectsRepository) : base(objectsRepository)
         {
             _lookUpObject = obj;
             _name = obj.ToString();
-            _isLookable = true;
+            _isLookable = false;
         }
     }
 }
