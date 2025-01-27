b28e68cd87abf41e71b186f3a99a63bbba770cff
Смирнов <maxah66@gmail.com>
2025-01-27 12:49:14 +0300

    AddPageController

---
+++ Interfaces/IPageController.cs
@@ -0,0 +1,19 @@
+﻿using PilotLookUp.Enums;
+using PilotLookUp.Model;
+using PilotLookUp.Objects;
+using PilotLookUp.View.UserControls;
+using PilotLookUp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilotLookUp.Interfaces
+{
+    internal interface IPageController
+    {
+        public void GoToPage(PagesName pageName);
+        public void CreatePage(PagesName pageName, PilotObjectHelper dataObj = null);
+    }
+}
