688ccd65b02e88e027b957e5cba669f458f50227
Смирнов <maxah66@gmail.com>
2025-06-27 22:32:57 +0300

    SelectorFix

--- src/App.cs
+++ src/App.cs
@@ -1,9 +1,11 @@
 ﻿using Ascon.Pilot.SDK;
 using Ascon.Pilot.SDK.Menu;
+using Ascon.Pilot.SDK.Toolbar;
 using Ascon.Pilot.Themes;
 using PilotLookUp.Objects;
 using PilotLookUp.Utils;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 
@@ -17,14 +19,25 @@ namespace PilotLookUp
     [Export(typeof(IMenu<DocumentFilesContext>))]
     [Export(typeof(IMenu<LinkedObjectsContext>))]
     [Export(typeof(IMenu<LinkedTasksContext2>))]
-
-    public class App : IMenu<MainViewContext>
-        , IMenu<ObjectsViewContext>
-        , IMenu<StorageContext>
-        , IMenu<TasksViewContext2>
-        , IMenu<DocumentFilesContext>
-        , IMenu<LinkedObjectsContext>
-        , IMenu<LinkedTasksContext2>
+    [Export(typeof(IToolbar<MainViewContext>))]
+    [Export(typeof(IToolbar<ObjectsViewContext>))]
+    [Export(typeof(IToolbar<TasksViewContext2>))]
+    [Export(typeof(IToolbar<DocumentFilesContext>))]
+    [Export(typeof(IToolbar<LinkedObjectsContext>))]
+    [Export(typeof(IToolbar<LinkedTasksContext2>))]
+    public class App : IMenu<MainViewContext>,
+        IMenu<ObjectsViewContext>,
+        IMenu<StorageContext>,
+        IMenu<TasksViewContext2>,
+        IMenu<DocumentFilesContext>,
+        IMenu<LinkedObjectsContext>,
+        IMenu<LinkedTasksContext2>,
+        IToolbar<MainViewContext>,
+        IToolbar<ObjectsViewContext>, 
+        IToolbar<TasksViewContext2>,
+        IToolbar<DocumentFilesContext>,
+        IToolbar<LinkedObjectsContext>,
+        IToolbar<LinkedTasksContext2>
     {
         private IObjectsRepository _objectsRepository;
         private ITabServiceProvider _tabServiceProvider;
@@ -33,7 +46,9 @@ namespace PilotLookUp
         public static ThemeNames Theme { get => _theme; }
 
         [ImportingConstructor]
-        public App(IObjectsRepository objectsRepository, ITabServiceProvider tabServiceProvider, IPilotDialogService pilotDialogService)
+        public App(IObjectsRepository objectsRepository,
+            ITabServiceProvider tabServiceProvider,
+            IPilotDialogService pilotDialogService)
         {
             AppDomain.CurrentDomain.AssemblyResolve += Resolver.ResolveAssembly;
             _objectsRepository = objectsRepository;
@@ -51,67 +66,45 @@ namespace PilotLookUp
             item.WithSubmenu().AddItem("LookDB", 1).WithHeader("LookDB");
             item.WithSubmenu().AddItem("Search", 2).WithHeader("Search");
         }
-        public void Build(IMenuBuilder builder, ObjectsViewContext context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
-        public void Build(IMenuBuilder builder, StorageContext context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
-        public void Build(IMenuBuilder builder, TasksViewContext2 context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
-        public void Build(IMenuBuilder builder, DocumentFilesContext context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
-        public void Build(IMenuBuilder builder, LinkedObjectsContext context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
-        public void Build(IMenuBuilder builder, LinkedTasksContext2 context)
-        {
-            SelectUpdater(context);
-            ContextButtunBuilder(builder);
-        }
+        public void Build(IMenuBuilder builder, ObjectsViewContext context) =>
+            ContextButtonBuilder(builder, context);
+
+        public void Build(IMenuBuilder builder, StorageContext context) =>
+            ContextButtonBuilder(builder, context);
+
+        public void Build(IMenuBuilder builder, TasksViewContext2 context) =>
+            ContextButtonBuilder(builder, context);
+
+        public void Build(IMenuBuilder builder, DocumentFilesContext context) =>
+            ContextButtonBuilder(builder, context);
+
+        public void Build(IMenuBuilder builder, LinkedObjectsContext context) =>
+            ContextButtonBuilder(builder, context);
+
+        public void Build(IMenuBuilder builder, LinkedTasksContext2 context) =>
+            ContextButtonBuilder(builder, context);
 
         // Event
-        public void OnMenuItemClick(string name, MainViewContext context)
-        {
+        public void OnMenuItemClick(string name, MainViewContext context) =>
             ItemClick(name);
-        }
-        public void OnMenuItemClick(string name, ObjectsViewContext context)
-        {
-            ItemClick(name);
-        }
-        public void OnMenuItemClick(string name, StorageContext context)
-        {
-            ItemClick(name);
-        }
-        public void OnMenuItemClick(string name, TasksViewContext2 context)
-        {
-            ItemClick(name);
-        }
-        public void OnMenuItemClick(string name, DocumentFilesContext context)
-        {
-            ItemClick(name);
-        }
-        public void OnMenuItemClick(string name, LinkedObjectsContext context)
-        {
-            ItemClick(name);
-        }
 
-        public void OnMenuItemClick(string name, LinkedTasksContext2 context)
-        {
+        public void OnMenuItemClick(string name, ObjectsViewContext context) =>
             ItemClick(name);
-        }
+
+        public void OnMenuItemClick(string name, StorageContext context) =>
+            ItemClick(name);
+
+        public void OnMenuItemClick(string name, TasksViewContext2 context) =>
+            ItemClick(name);
+
+        public void OnMenuItemClick(string name, DocumentFilesContext context) =>
+            ItemClick(name);
+
+        public void OnMenuItemClick(string name, LinkedObjectsContext context) =>
+            ItemClick(name);
+
+        public void OnMenuItemClick(string name, LinkedTasksContext2 context) =>
+            ItemClick(name);
 
         private void ItemClick(string name)
         {
@@ -136,46 +129,65 @@ namespace PilotLookUp
 
         }
 
-        private void ContextButtunBuilder(IMenuBuilder builder)
+        private void ContextButtonBuilder(IMenuBuilder builder, MarshalByRefObject context)
         {
+            SelectUpdater(context);
             builder.AddItem("LookSelected", 0).WithHeader("LookSelected");
         }
 
         private void SelectUpdater(MarshalByRefObject context)
         {
-            var pilotObjectMap = new PilotObjectMap(_objectsRepository);
-            _convertSelection = new ObjectSet(null);
+            var map = new PilotObjectMap(_objectsRepository);
 
-            switch (context)
+            IEnumerable<object>? raw = context switch
             {
-                case MainViewContext mainViewContext:
-                    return;
+                ObjectsViewContext c => c.SelectedObjects?.Cast<object>(),
+                DocumentFilesContext c => c.SelectedObjects?.Cast<object>(),
+                LinkedObjectsContext c => c.SelectedObjects?.Cast<object>(),
 
-                case ObjectsViewContext objectsViewContext:
-                    var selectedDO = objectsViewContext.SelectedObjects?.Select(i => pilotObjectMap.Wrap(i)).ToList();
-                    _convertSelection.AddRange(selectedDO.Any() ? selectedDO : _convertSelection);
-                    return;
+                TasksViewContext2 c => c.SelectedTasks?.Cast<object>(),
+                LinkedTasksContext2 c => c.SelectedTasks?.Cast<object>(),
 
-                case TasksViewContext2 tasksViewContext:
-                    var selectedT = tasksViewContext.SelectedTasks?.Select(i => pilotObjectMap.Wrap(i)).ToList();
-                    _convertSelection.AddRange(selectedT.Any() ? selectedT : _convertSelection);
-                    return;
+                _ => null
+            };
 
-                case DocumentFilesContext documentFilesContext:
-                    var selectedF = documentFilesContext.SelectedObjects?.Select(i => pilotObjectMap.Wrap(i)).ToList();
-                    _convertSelection.AddRange(selectedF.Any() ? selectedF : _convertSelection);
-                    break;
-
-                case LinkedObjectsContext linkedObjectsContext:
-                    var selectedLO = linkedObjectsContext.SelectedObjects?.Select(i => pilotObjectMap.Wrap(i)).ToList();
-                    _convertSelection.AddRange(selectedLO.Any() ? selectedLO : _convertSelection);
-                    break;
-
-                case LinkedTasksContext2 linkedTasksContext:
-                    var selectedLT = linkedTasksContext.SelectedTasks?.Select(i => pilotObjectMap.Wrap(i)).ToList();
-                    _convertSelection.AddRange(selectedLT.Any() ? selectedLT : _convertSelection);
-                    break;
+            if (raw?.Any() == true)
+            {
+                _convertSelection = new ObjectSet(null);
+                _convertSelection.AddRange(raw.Select(map.Wrap));
             }
         }
+
+        // Juts for stable update
+        public void Build(IToolbarBuilder builder, ObjectsViewContext context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, ObjectsViewContext context) { }
+
+
+        public void Build(IToolbarBuilder builder, TasksViewContext2 context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, TasksViewContext2 context) { }
+
+        public void Build(IToolbarBuilder builder, DocumentFilesContext context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, DocumentFilesContext context) { }
+
+        public void Build(IToolbarBuilder builder, LinkedObjectsContext context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, LinkedObjectsContext context) { }
+
+        public void Build(IToolbarBuilder builder, LinkedTasksContext2 context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, LinkedTasksContext2 context) { }
+
+        public void Build(IToolbarBuilder builder, MainViewContext context) =>
+            SelectUpdater(context);
+
+        public void OnToolbarItemClick(string name, MainViewContext context) { }
     }
 }
