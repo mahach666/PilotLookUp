c79d651c31ecdaa4f267d287ff8e2da3728dd250
Смирнов <maxah66@gmail.com>
2025-07-17 12:53:53 +0300

    attrFix

--- src/Model/Services/DataObjectService.cs
+++ src/Model/Services/DataObjectService.cs
@@ -2,6 +2,7 @@
 using PilotLookUp.Contracts;
 using PilotLookUp.Interfaces;
 using PilotLookUp.Objects.TypeHelpers;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,7 +21,9 @@ namespace PilotLookUp.Model.Services
             {
                 Name = attr.Name,
                 Title = attr.Title,
-                Value = objAttr.TryGetValue(attr.Name, out var value) ? value?.ToString() : string.Empty,
+                Value = objAttr.TryGetValue(attr.Name, out var value)
+                        ? FormatValue(value)
+                        : string.Empty,
                 IsObligatory = attr.IsObligatory.ToString(),
                 IsService = attr.IsService.ToString(),
                 Type = attr.Type.ToString(),
@@ -41,6 +44,20 @@ namespace PilotLookUp.Model.Services
                 }));
 
             return res;
+        }
+
+        private static string FormatValue(object value)
+        {
+            if (value is string str)
+                return str;
+
+            if (value is IEnumerable enumerable)
+            {
+                return string.Join("; ", enumerable
+                    .Cast<object>()
+                    .Select(item => item?.ToString() ?? string.Empty));
+            }
+            return value?.ToString() ?? string.Empty;
         }
     }
 }
